425095195873525d68bbb2045d7421c9c4267102
Michael Beaton <beatonma@gmail.com>
2021-09-09 23:47:45 +0100

    AppTheme now chooses colors from those chosen for orbital bodies.

--- app/src/main/java/org/beatonma/orbitalslivewallpaper/app/AppTheme.kt
+++ app/src/main/java/org/beatonma/orbitalslivewallpaper/app/AppTheme.kt
@@ -8,17 +8,41 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Shapes
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.insets.ProvideWindowInsets
+import org.beatonma.orbitalslivewallpaper.dataStore
+import org.beatonma.orbitalslivewallpaper.orbitals.options.ColorOptions
+import org.beatonma.orbitalslivewallpaper.orbitals.options.Settings
+import org.beatonma.orbitalslivewallpaper.orbitals.options.getSavedColors
+import org.beatonma.orbitalslivewallpaper.orbitals.renderer.compose.util.toComposeColor
 
 @Composable
 fun AppTheme(
     isDark: Boolean = isSystemInDarkTheme() || Build.VERSION.SDK_INT < Build.VERSION_CODES.Q,
     content: @Composable () -> Unit,
 ) {
+    val colorsOptions by getSavedColors(
+        LocalContext.current.dataStore(Settings.Wallpaper)
+    ).collectAsState(
+        initial = ColorOptions()
+    )
+
     MaterialTheme(
-        colors = if (isDark) darkColors() else lightColors(),
+        colors = if (isDark) {
+            darkColors(
+                colorsOptions.bodies.random().colors(),
+                colorsOptions.bodies.random().colors()
+            )
+        } else {
+            lightColors(
+                colorsOptions.bodies.random().colors(),
+                colorsOptions.bodies.random().colors()
+            )
+        },
         shapes = Shapes(
             small = CutCornerShape(4.dp),
             medium = CutCornerShape(8.dp),
@@ -35,5 +59,22 @@ fun AppTheme(
 }
 
 
-private fun lightColors() = androidx.compose.material.lightColors()
-private fun darkColors() = androidx.compose.material.darkColors()
+private fun lightColors(
+    primaryColors: Array<Int>,
+    secondaryColors: Array<Int>,
+) = androidx.compose.material.lightColors(
+    primary = primaryColors[primaryColors.size / 2].toComposeColor(),
+    primaryVariant = primaryColors[primaryColors.size / 4].toComposeColor(),
+    secondary = secondaryColors[secondaryColors.size / 2].toComposeColor(),
+    secondaryVariant = secondaryColors[secondaryColors.size / 4].toComposeColor(),
+)
+
+private fun darkColors(
+    primaryColors: Array<Int>,
+    secondaryColors: Array<Int>,
+) = androidx.compose.material.darkColors(
+    primary = primaryColors[primaryColors.size / 2].toComposeColor(),
+    primaryVariant = primaryColors[primaryColors.size / 4].toComposeColor(),
+    secondary = secondaryColors[secondaryColors.size / 2].toComposeColor(),
+    secondaryVariant = secondaryColors[secondaryColors.size / 4].toComposeColor(),
+)
--- app/src/main/java/org/beatonma/orbitalslivewallpaper/orbitals/options/OptionsDatastore.kt
+++ app/src/main/java/org/beatonma/orbitalslivewallpaper/orbitals/options/OptionsDatastore.kt
@@ -11,7 +11,6 @@ import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.core.stringSetPreferencesKey
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
@@ -34,52 +33,16 @@ fun getSavedOptionsSync(dataStore: DataStore<Preferences>): Options = runBlockin
 @OptIn(ExperimentalTime::class)
 fun getSavedOptions(dataStore: DataStore<Preferences>): Flow<Options> {
     return dataStore.data.map { preferences ->
-        val physics = with (PhysicsKeys) {
-            PhysicsOptions(
-                maxEntities = preferences[maxEntities] ?: 25,
-                systemGenerators = preferences[systemGenerators]
-                    ?.map { SystemGenerator.valueOf(it) }
-                    ?: listOf(
-                        SystemGenerator.Gauntlet,
-                        SystemGenerator.Randomized,
-                        SystemGenerator.StarSystem,
-                    ),
-                gravityMultiplier = preferences[gravityMultiplier] ?: 1f,
-                collisionStyle = preferences[collisionStyle]
-                    ?.let { CollisionStyle.valueOf(it) }
-                    ?: CollisionStyle.None,
-                tickDelta = Duration.seconds(preferences[tickDelta] ?: 1),
-            )
-        }
-
-        val colors = with(ColorKeys) {
-            ColorOptions(
-                background = preferences[background] ?: Color.BLACK,
-                foregroundAlpha = preferences[foregroundAlpha] ?: 1f,
-                bodies = preferences[bodies]
-                    ?.map { ObjectColors.valueOf(it) }
-                    ?: listOf(
-                        ObjectColors.Red,
-                        ObjectColors.Purple,
-                    ),
-            )
-        }
-
-        val visuals = with(VisualKeys) {
-            VisualOptions(
-                colorOptions = colors,
-                showTraceLines = preferences[showTraceLines] ?: false,
-                traceLineLength = preferences[traceLineLength] ?: 50,
-                showAcceleration = preferences[showAcceleration] ?: false,
-                drawStyle = preferences[drawStyle]
-                    ?.let { DrawStyle.valueOf(it) }
-                    ?: DrawStyle.Solid,
-                strokeWidth = (preferences[strokeWidth] ?: 4f).dp
-            )
-        }
+        val physics = loadPhysicsOptions(preferences)
+        val colors = loadColors(preferences)
+        val visuals = loadVisualOptions(preferences, colors)
 
         Options(physics, visuals)
     }
+}
+
+fun getSavedColors(dataStore: DataStore<Preferences>): Flow<ColorOptions> {
+    return dataStore.data.map(::loadColors)
 }
 
 suspend fun <T> updateOption(
@@ -113,4 +76,55 @@ object ColorKeys {
     val background = intPreferencesKey("background_color")
     val bodies = stringSetPreferencesKey("body_colors")
     val foregroundAlpha = floatPreferencesKey("foreground_alpha")
+}
+
+private fun loadColors(preferences: Preferences): ColorOptions =
+    with(ColorKeys) {
+        ColorOptions(
+            background = preferences[background] ?: Color.BLACK,
+            foregroundAlpha = preferences[foregroundAlpha] ?: 1f,
+            bodies = preferences[bodies]
+                ?.map { ObjectColors.valueOf(it) }
+                ?: listOf(
+                    ObjectColors.Red,
+                    ObjectColors.Purple,
+                ),
+        )
+    }
+
+private fun loadVisualOptions(
+    preferences: Preferences,
+    colors: ColorOptions = loadColors(preferences),
+): VisualOptions = with(VisualKeys) {
+    VisualOptions(
+        colorOptions = colors,
+        showTraceLines = preferences[showTraceLines] ?: false,
+        traceLineLength = preferences[traceLineLength] ?: 50,
+        showAcceleration = preferences[showAcceleration] ?: false,
+        drawStyle = preferences[drawStyle]
+            ?.let { DrawStyle.valueOf(it) }
+            ?: DrawStyle.Solid,
+        strokeWidth = (preferences[strokeWidth] ?: 4f).dp
+    )
+}
+
+@OptIn(ExperimentalTime::class)
+private fun loadPhysicsOptions(
+    preferences: Preferences,
+): PhysicsOptions = with(PhysicsKeys) {
+    PhysicsOptions(
+        maxEntities = preferences[maxEntities] ?: 25,
+        systemGenerators = preferences[systemGenerators]
+            ?.map { SystemGenerator.valueOf(it) }
+            ?: listOf(
+                SystemGenerator.Gauntlet,
+                SystemGenerator.Randomized,
+                SystemGenerator.StarSystem,
+            ),
+        gravityMultiplier = preferences[gravityMultiplier] ?: 1f,
+        collisionStyle = preferences[collisionStyle]
+            ?.let { CollisionStyle.valueOf(it) }
+            ?: CollisionStyle.None,
+        tickDelta = Duration.seconds(preferences[tickDelta] ?: 1),
+    )
 }
