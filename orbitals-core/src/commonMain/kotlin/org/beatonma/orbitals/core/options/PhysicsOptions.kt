eaf5a364c6a1c1b402eae84ae0fcfd4ddfc11791
Michael Beaton <beatonma@gmail.com>
2024-01-14 16:45:59 +0000

    Default options tweaked

--- orbitals-core/src/commonMain/kotlin/org/beatonma/orbitals/core/options/PhysicsOptions.kt
+++ orbitals-core/src/commonMain/kotlin/org/beatonma/orbitals/core/options/PhysicsOptions.kt
@@ -4,18 +4,20 @@ import org.beatonma.orbitals.core.engine.SystemGenerator
 import org.beatonma.orbitals.core.physics.DefaultG
 import org.beatonma.orbitals.core.physics.Density
 import kotlin.time.Duration
-import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
 
 data class PhysicsOptions(
     val autoAddBodies: Boolean = true,
-    val maxFixedBodyAge: Duration = 1.minutes,
+    val maxFixedBodyAge: Duration = 30.seconds,
     val maxEntities: Int = 50,
     val systemGenerators: Set<SystemGenerator> = setOf(
+        SystemGenerator.Asteroids,
+        SystemGenerator.Randomized,
         SystemGenerator.StarSystem,
     ),
     val gravityMultiplier: Float = 1f,
-    val collisionStyle: CollisionStyle = CollisionStyle.None,
-    val bodyDensity: Density = Density(.5f),
+    val collisionStyle: CollisionStyle = CollisionStyle.Merge,
+    val bodyDensity: Density = Density(1f),
 ) {
     val G = DefaultG * gravityMultiplier
 }
