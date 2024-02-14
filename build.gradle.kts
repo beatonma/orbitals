b54588406511b0e021840540c6e3be6419fe54ca
Michael Beaton <beatonma@gmail.com>
2024-02-14 14:33:51 +0000

    Version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id(libs.plugins.kotlin.multiplatform.get().pluginId) apply false
-    id(libs.plugins.compose.get().pluginId) version libs.versions.compose.multiplatform apply false
     id(libs.plugins.android.library.get().pluginId) apply false
     id(libs.plugins.android.application.get().pluginId) apply false
-    id(libs.plugins.kotlin.jvm.get().pluginId) apply false
+    alias(libs.plugins.compose) apply false
 }
